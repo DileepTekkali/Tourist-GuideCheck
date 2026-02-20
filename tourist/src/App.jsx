edb5fc3cced61df817aeed243c4656708318208a
Tekkali Dileep kumar <dileepkumar66682@gmail.com>
2026-02-20 17:56:18 +0530

    updated inc

--- tourist/src/App.jsx
+++ tourist/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>tourist spots</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 4)}>
-          count is {count}
+          increase count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
