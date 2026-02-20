957ee656d9315b7e7415bf5049791d7412348faa
Tekkali Dileep kumar <dileepkumar66682@gmail.com>
2026-02-20 17:52:03 +0530

    updated name

--- tourist/src/App.jsx
+++ tourist/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>tourist</h1>
+      <h1>tourist spots</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 4)}>
           count is {count}
